3d50ced73697659fcf056445efc4c2a162bec79b
nileka3113 <nilekakavisandi@gmail.com>
2025-12-07 20:44:02 +0530

    updated requirements.txt

--- backend/main.py
+++ backend/main.py
@@ -377,4 +377,4 @@ def premium_yearly_page():
 if __name__ == "__main__":
 # Use Render's dynamic port, host must be 0.0.0.0
  port = int(os.environ.get("PORT", 5000))
- app.run(host="0.0.0.0", port=port, debug=True)
+ app.run(host="0.0.0.0", port=port, debug=False)
